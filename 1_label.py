6d5fb157819abf489626aa6de1f285938db36736
aspriya <ashanpriyadarshana@gmail.com>
2018-02-27 00:29:08 +0530

    updata 1_label to a login screen

--- 1_label.py
+++ 1_label.py
@@ -1,10 +1,23 @@
 from kivy.app import App
 from kivy.uix.button import Label
+from kivy.uix.gridlayout import GridLayout
+from kivy.uix.textinput import TextInput
 
 class SomeClassApp(App):
 
 	def build(self):
-		return Label()
+		return LoginScreen()
 
-c1 = SomeClassApp()
-c1.run()
+class LoginScreen(GridLayout):
+	def __init__(self, **kwards):
+		super(LoginScreen, self).__init__(**kwards)
+		self.cols = 2
+		self.add_widget(Label(text='User Name'))
+		self.username = TextInput(multiline=False)
+		self.add_widget(self.username)
+		self.add_widget(Label(text='password'))
+		self.password = TextInput(password=True, multiline=False)
+		self.add_widget(self.password)
+
+if __name__ == '__main__':
+	SomeClassApp().run()
